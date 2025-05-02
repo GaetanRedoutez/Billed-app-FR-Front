f3fc0cc1202f4282ba3fb80c3c85c191f2d6e132
Gaetan Redoutez <gaetan.redoutez@preconys.com>
2025-05-02 09:04:51 +0200

    feat(NewBill): validate file type for uploads

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -19,15 +19,22 @@ export default class NewBill {
   }
   handleChangeFile = (e) => {
     e.preventDefault();
-    const file = this.document.querySelector(`input[data-testid="file"]`)
-      .files[0];
-    const filePath = e.target.value.split(/\\/g);
-    const fileName = filePath[filePath.length - 1];
+    const fileInput = this.document.querySelector(`input[data-testid="file"]`);
+    const file = fileInput.files[0];
+    const allowedExtensions = ["jpg", "jpeg", "png"];
+    const fileName = file.name.toLowerCase();
+    const fileExtension = fileName.split(".").pop();
     const formData = new FormData();
     const email = JSON.parse(localStorage.getItem("user")).email;
     formData.append("file", file);
     formData.append("email", email);
 
+    if (file) {
+      if (!allowedExtensions.includes(fileExtension)) {
+        alert("Seuls les fichiers .jpg, .jpeg, .png sont autorisés.");
+        fileInput.value = "";
+      }
+    }
     this.store
       .bills()
       .create({
