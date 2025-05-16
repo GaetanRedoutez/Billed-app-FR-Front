6b18b7f08cbccfd61da245848708212ea7851f31
Gaetan Redoutez <gaetan.redoutez@preconys.com>
2025-05-16 10:44:38 +0200

    test(Bills): add tests for fetching bills and handling API errors

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -3,14 +3,16 @@
  */
 
 import { screen, waitFor } from "@testing-library/dom";
-import BillsUI from "../views/BillsUI.js";
-import { bills } from "../fixtures/bills.js";
-import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
-import { localStorageMock } from "../__mocks__/localStorage.js";
-
-import router from "../app/Router.js";
 import userEvent from "@testing-library/user-event";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store";
+import router from "../app/Router";
+import { ROUTES, ROUTES_PATH } from "../constants/routes";
+import { bills } from "../fixtures/bills";
+import BillsUI from "../views/BillsUI.js";
 import Bills from "../containers/Bills.js";
+
+jest.mock("../app/Store", () => mockStore);
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -91,6 +93,76 @@ describe("Given I am connected as an employee", () => {
       expect(img).toBeTruthy();
       expect(img.getAttribute("src")).toBe(expectedUrl);
       expect(img.getAttribute("alt")).toBe("Bill");
+    });
+  });
+});
+
+describe("Given I am a user connected as Employee", () => {
+  describe("When I navigate to Bills Page", () => {
+    test("fetches bills from mock API GET", async () => {
+      localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+          email: "a@a",
+        })
+      );
+
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Bills);
+
+      await waitFor(() => screen.getByText("Mes notes de frais"));
+      const tableRows = screen.getAllByTestId("icon-eye");
+      expect(tableRows.length).toBeGreaterThan(0);
+    });
+
+    describe("When an error occurs on API", () => {
+      beforeEach(() => {
+        jest.spyOn(mockStore, "bills");
+        Object.defineProperty(window, "localStorage", {
+          value: localStorageMock,
+        });
+        window.localStorage.setItem(
+          "user",
+          JSON.stringify({
+            type: "Employee",
+            email: "a@a",
+          })
+        );
+        const root = document.createElement("div");
+        root.setAttribute("id", "root");
+        document.body.append(root);
+        router();
+      });
+
+      test("fails with 404 error message", async () => {
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list: () => Promise.reject(new Error("Erreur 404")),
+          };
+        });
+
+        window.onNavigate(ROUTES_PATH.Bills);
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 404/);
+        expect(message).toBeTruthy();
+      });
+
+      test("fails with 500 error message", async () => {
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list: () => Promise.reject(new Error("Erreur 500")),
+          };
+        });
+
+        window.onNavigate(ROUTES_PATH.Bills);
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 500/);
+        expect(message).toBeTruthy();
+      });
     });
   });
 });
