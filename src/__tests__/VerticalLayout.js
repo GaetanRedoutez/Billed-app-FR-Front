c500f54152dc42e6eec89a319880c234abe85268
Gaetan Redoutez <gaetan.redoutez@preconys.com>
2025-05-23 14:31:18 +0200

    test(VerticalLayout): enhance tests for user roles and localStorage scenarios

--- src/__tests__/VerticalLayout.js
+++ src/__tests__/VerticalLayout.js
@@ -9,13 +9,41 @@ import { localStorageMock } from "../__mocks__/localStorage.js";
 describe("Given I am connected as Employee", () => {
   test("Then Icons should be rendered", () => {
     Object.defineProperty(window, "localStorage", { value: localStorageMock });
-    const user = JSON.stringify({
-      type: "Employee",
-    });
+    const user = JSON.stringify({ type: "Employee" });
     window.localStorage.setItem("user", user);
     const html = VerticalLayout(120);
     document.body.innerHTML = html;
     expect(screen.getByTestId("icon-window")).toBeTruthy();
     expect(screen.getByTestId("icon-mail")).toBeTruthy();
+    expect(screen.getByTestId("layout-disconnect")).toBeTruthy();
+  });
+});
+
+describe("Given I am connected as non-Employee", () => {
+  test("Then only disconnect icon should be rendered", () => {
+    Object.defineProperty(window, "localStorage", { value: localStorageMock });
+    const user = JSON.stringify({ type: "Manager" });
+    window.localStorage.setItem("user", user);
+
+    const html = VerticalLayout(120);
+    document.body.innerHTML = html;
+
+    expect(screen.queryByTestId("icon-window")).toBeNull();
+    expect(screen.queryByTestId("icon-mail")).toBeNull();
+    expect(screen.getByTestId("layout-disconnect")).toBeTruthy();
+  });
+});
+
+describe("Given there is no user in localStorage", () => {
+  test("Then only disconnect icon should be rendered", () => {
+    Object.defineProperty(window, "localStorage", { value: localStorageMock });
+    window.localStorage.removeItem("user");
+
+    const html = VerticalLayout(120);
+    document.body.innerHTML = html;
+
+    expect(screen.queryByTestId("icon-window")).toBeNull();
+    expect(screen.queryByTestId("icon-mail")).toBeNull();
+    expect(screen.getByTestId("layout-disconnect")).toBeTruthy();
   });
 });
